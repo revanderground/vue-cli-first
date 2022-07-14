32799397dfc8de6141b8d647eae26210f17252ca
vtnboolean <vnthuanx@gmail.com>
2022-07-14 23:22:35 +0200

    Jumbotron added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Titolo />
+    <Jumbotron />
   </div>
 </template>
 
 <script>
 import Titolo from './components/Titolo.vue'
+import Jumbotron from './components/Jumbotron.vue'
 
 export default {
   name: 'App',
   components: {
-    Titolo
+    Titolo,
+    Jumbotron
   }
 }
 </script>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="jumbotron">
+  <h1>Sasuke Uchia</h1>
+  <p>Seeking for Revenge</p>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.jumbotron{
+    background-image: url('../assets/DGK4MlhXkAEvTdb.jpg');
+    height: 400px;
+}
+
+</style>
