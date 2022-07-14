59e7ceb23f9940b2a3bcbf6f63f678da738d7b0d
vtnboolean <vnthuanx@gmail.com>
2022-07-14 22:51:17 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Titolo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Titolo from './components/Titolo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Titolo
   }
 }
 </script>
---
+++ src/components/Titolo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>La mia prima app con Vue CLI!</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
